6db5b465dec1245570e4ecae369dc16c25e0569c
John Peroutka <hexajohnart@gmail.com>
2023-11-19 18:18:53 -0600

    Add import for entry1.dart in main_menu.dart

--- lib/src/story/main_menu.dart
+++ lib/src/story/main_menu.dart
@@ -1,5 +1,6 @@
 import 'package:september_flutter/src/core/app.dart';
 import 'package:september_flutter/src/core/messages.dart';
+import 'package:september_flutter/src/mainland/rooms/entry1.dart';
 import 'package:september_flutter/src/story/rooms/room_701.dart';
 
 class MainMenuModule extends PointOfInterest {
@@ -7,7 +8,9 @@ class MainMenuModule extends PointOfInterest {
   Message? evaluateSpecial(TextInteraction input) {
     int index = 0;
 
-    if (input.lookFor([SoftToken('mainland')])) {
+    if (input.lookFor([
+      SoftToken(['mainland'])
+    ])) {
       return _startMainland();
     }
 
@@ -24,7 +27,9 @@ class MainMenuModule extends PointOfInterest {
         AppInterface.setCrumb(StoryCrumb(['game', 'playing', 'adventure', 'story'], () {}));
         return Message('What do you want to ${input.segments.first}?', bold: true);
       } else {
-        if (input.lookFor([SoftToken('mainland')])) {
+        if (input.lookFor([
+          SoftToken(['mainland'])
+        ])) {
           return _startMainland();
         }
         if (input.lookFor([SoftToken.game()])) {
@@ -45,11 +50,12 @@ class MainMenuModule extends PointOfInterest {
         bold: true);
   }
 
-  Message _startMainland() {
+  Message? _startMainland() {
     // AppInterface.newGame();
-    AppInterface.changeContext(TutorialShipRoom());
-    return Message(
-        '''The sea has rested for several days now, while the sea within me rages. The storm in my heart roars at the idea I alone survived this catastrophe. I spent quite some hours on deck staring along the horizon, blasted by the salt spray.  I have been well in light of the possibilities that surround me, although weak. Even keeping a record of my activities often exhausts me to the point of despair. More than once I have pondered whether anyone will ever review these entries, or whether I wish them to. I imagine my diary will be placed in the hands of my beloved, so she may learn what I have gone through to place it there and feel my love, for it has been more than the incredible journey we dreamed of. Truth be told, if I say not a word as the black, wretched loneliness creeps into my quarters, I may hold fast to the hope I will return to her side. Alas, my strength wanes.''',
-        bold: true);
+    AppInterface.changeContext(MLEntry1());
+    return Message.invisible();
+    // return Message(
+    // '''The sea has rested for several days now, while the sea within me rages. The storm in my heart roars at the idea I alone survived this catastrophe.''',
+    // bold: true);
   }
 }
