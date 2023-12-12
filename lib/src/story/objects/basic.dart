e05aded447e18a14c0f65b922f9069c340e4f216
John Peroutka <hexajohnart@gmail.com>
2023-12-11 19:27:07 -0600

    Refactor basic entities

--- lib/src/story/objects/basic.dart
+++ lib/src/story/objects/basic.dart
@@ -2,9 +2,11 @@ import 'package:september_flutter/src/core/app.dart';
 import 'package:september_flutter/src/core/grammar/sentence.dart';
 import 'package:september_flutter/src/core/grammar/token.dart';
 import 'package:september_flutter/src/core/grammar/token_soft.dart';
+import 'package:september_flutter/src/core/interaction/interface.dart';
 import 'package:september_flutter/src/core/messages.dart';
+import 'package:september_flutter/src/core/world/entity.dart';
 
-class BasicSwitch extends PointOfInterest {
+class BasicSwitch extends EntityInterface {
   BasicSwitch(
       {super.hintText = 'A switch of sorts', super.names = const SoftToken(['switch', 'lights']), super.name = ''});
   bool state = false;
@@ -15,7 +17,7 @@ class BasicSwitch extends PointOfInterest {
   }
 }
 
-class BasicWindow extends PointOfInterest {
+class BasicWindow extends EntityInterface {
   BasicWindow(
       {super.hintText = 'Look outside!', super.names = const SoftToken(['window', 'windows']), super.name = ''});
   bool open = true;
@@ -26,7 +28,7 @@ class BasicWindow extends PointOfInterest {
   }
 }
 
-class BasicDoor extends PointOfInterest {
+class BasicDoor extends EntityInterface {
   BasicDoor(
       {super.hintText = 'Maybe I should knock?', super.names = const SoftToken(['door', 'entrance']), super.name = ''});
   bool open = false;
@@ -38,56 +40,7 @@ class BasicDoor extends PointOfInterest {
   }
 }
 
-class BasicItem extends PointOfInterest {
-  BasicItem(
-      {super.hintText = 'A knickknack of sorts',
-      super.names = const SoftToken(['item', 'thing']),
-      super.name = '',
-      Map<SoftToken, Message? Function(TextInteraction)?> specialResponses = const {}}) {
-    this.specialResponses = specialResponses;
-  }
-  bool state = false;
-
-  // Message? Function(TextInteraction)? function;
-
-  @override
-  Message? evaluate(TextInteraction input) {
-    // print('evaluating ${input.toDistilledTokens()} [${input.raw}] on $name');
-    final Idea sentence = input.toSentence().ideas.first;
-    // print('evaluating sentence ${sentence} [${input.raw}] on $name');
-    final Token? predicate = sentence.predicate;
-    // print('evaluating predicate ${predicate} [${input.raw}] on $name');
-    final Token? directObject = sentence.directObject;
-    // print('evaluating subject ${subject} [${input.raw}] on $name');
-
-    /// If the sentence doesn't have a predicate, return null.
-    if (predicate == null) return null;
-
-    /// If the sentence doesn't have a subject, return null.
-    if (directObject == null) return null;
-
-    /// Use the sentence subject to determine if the player is talking about
-    /// this item. If not, return null.
-    if (!names.contains(directObject)) {
-      return null;
-    }
-
-    /// Look for a special response that matches the sentence predicate. If
-    /// there is one, invoke it and return the result.
-    try {
-      Message? Function(TextInteraction)? out =
-          specialResponses.entries.firstWhere((element) => element.key.contains(predicate)).value;
-
-      if (out != null) return out(input);
-    } catch (e) {
-      /// If the sentence predicate doesn't match any of the special responses,
-      /// return null.
-      return null;
-    }
-  }
-}
-
-class BasicScenery extends BasicItem {
+class BasicScenery extends Entity {
   BasicScenery(
       {super.hintText = 'A place of sorts', super.names = const SoftToken(['cliff', 'rocks']), super.name = ''});
 }
